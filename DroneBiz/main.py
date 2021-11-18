4054b5db0bf1b9822fa94ff2ffe8b165ce2018fc
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2021-11-18 14:57:11 +0000

    Connected with repl test

--- DroneBiz/main.py
+++ DroneBiz/main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template("home.html")
+
 
 @app.route('/contact')
 def contact():
@@ -12,4 +14,4 @@ def contact():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
