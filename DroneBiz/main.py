fe44f3109c962feeda60c461d750ff7d1131a2eb
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2021-11-19 14:42:59 +0000

    added title variable

--- DroneBiz/main.py
+++ DroneBiz/main.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", title="home")
 
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", title="contact")
 
 
 if __name__ == '__main__':
